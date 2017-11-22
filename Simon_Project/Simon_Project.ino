b4831944ffbda6bccd4d53ca6fd48393f53335ef
ehuang2 <33882863+ehuang2@users.noreply.github.com>
2017-11-22 12:16:26 -0500

    Update Simon_Project.ino

--- Simon_Project/Simon_Project.ino
+++ Simon_Project/Simon_Project.ino
@@ -1,16 +1,47 @@
+//LED Pins
+
+//Button Pins
+
+
 void setup() 
 {
-  
+  //Generate array  
 
 }
 
 void loop() 
 {
-  
+  //Play the game
 
 }
 
 
 ///////////////// Put Functions Below This //////////////////////
 
+void randomArray()
+{
+  for(c = 0; c < 20; c++)
+  {
+    //Generate an array of 20 random numbers from 0-4
+    ranArray[c] = random(5);
+  }
+}
 
+void game()
+{
+  for(c = 0; c < 20; c++)
+  {
+    //Blink LED associated with ranArray[c]
+
+    //if user presses wrong button
+    if()
+    {
+      //flash all LEDs and play long sound indicating lost
+    }
+    else
+    {
+      //Increment c to add another LED
+      c++
+    }
+  }
+}
