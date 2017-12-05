55ae72cb6cf85d052a24adaf5fc003f926917f05
paulfnwall <paulfnwall@gmail.com>
2017-12-05 14:55:08 -0500

    Update incorrectAnswer.ino

--- incorrectAnswer.ino
+++ incorrectAnswer.ino
@@ -3,13 +3,15 @@ void incorrectAnswer ()
 { 
   //Reset turn to -1 to reset the game
   turn = -1;
+  
   /*Knight rider: Turn on one pin, turn on next pin, turn off previous pin
   Goes forward and back 3 times from blue LED to white LED
   Play a specific sound depending on which LED lights up
   Uses pinArray for LEDs and wrongNotes for sounds*/
   for(int k = 0; k < 3; k++)
   {
-    for(int count=0;count<4;count++) {
+    for(int count=0;count<4;count++)
+    {
      digitalWrite(pinArray[count], HIGH);
      playTone(wrongNotes[count], 50);
      delay(25);
@@ -19,7 +21,9 @@ void incorrectAnswer ()
      digitalWrite(pinArray[count], LOW);
      delay(35);
     }
-    for(int count=4;count>0;count--) {
+    
+    for(int count=4;count>0;count--) 
+    {
      digitalWrite(pinArray[count], HIGH);
      playTone(wrongNotes[count], 50);
      delay(25);
