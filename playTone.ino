e612f169438f4343abf166ef21c0ce26ef448cec
paulfnwall <paulfnwall@gmail.com>
2017-12-05 14:59:28 -0500

    Update playTone.ino

--- playTone.ino
+++ playTone.ino
@@ -1,11 +1,18 @@
+/* 
+This function was taken from the internet to make 
+playing tones easier. This lets you play a tone for 
+a specified amount of time in one line of code, 
+opposed to the normal tone function where you need 
+to turn the tone on in one line and off in another.
+*/
 void playTone(int tone, int duration) 
 {
  for (long i = 0; i < duration * 1000L; i += tone * 2) 
  {
-   //Set speakerPin to HIGH to play the tone
+   //Start the tone
    digitalWrite(speakerPin, HIGH);
    delayMicroseconds(tone);
-   //Set speakerPin to LOW to turn off the tone
+   //Stop the tone
    digitalWrite(speakerPin, LOW);
    delayMicroseconds(tone);
  }
